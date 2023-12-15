4fe7292c7f2e3bc08719120e1f45fbf9fcc8dc99
Matt Ozuna <m.ozuna22@gmail.com>
2023-12-15 12:37:59 -0500

    completed part 1

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-"""Flask app for Cupcakes"""
+from flask import Flask, render_template, flash, redirect, render_template
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "oh-so-secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///cupcake"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+connect_db(app)
--- models.py
+++ models.py
@@ -1,1 +1,29 @@
-"""Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    
+    flavor = db.Column(db.Text,
+                       nullable=False)
+
+    size = db.Column(db.Text,
+                     nullable=False)
+    
+    rating = db.Column(db.Float,
+                       nullable=False)
+    
+    image = db.Column(db.Text,
+                      nullable=False,
+                      default='https://thestayathomechef.com/wp-content/uploads/2017/12/Most-Amazing-Chocolate-Cupcakes-1-small.jpg')
